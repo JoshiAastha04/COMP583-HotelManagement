e0d116f35137f6e4c449b8d43c74176b4736dee1
Aastha Joshi <157534481+JoshiAastha04@users.noreply.github.com>
2026-02-19 20:45:00 -0800

    Initialize Spring Boot backend and React frontend

---
+++ backend/src/test/java/edu/csun/hotel_backend/HotelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.csun.hotel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
